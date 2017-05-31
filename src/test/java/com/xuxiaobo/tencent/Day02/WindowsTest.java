e9736100fbf449d8aac1a9cf715001431d054433
XUXIAOBO_COMPANY <234508021@qq.com>
2017-05-31 15:43:24 +0800

    CODE UPDATE!!!!

--- src/test/java/com/xuxiaobo/tencent/Day02/WindowsTest.java
+++ src/test/java/com/xuxiaobo/tencent/Day02/WindowsTest.java
@@ -2,12 +2,16 @@ package com.xuxiaobo.tencent.Day02;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Administrator on 2017/4/27 0027.
@@ -45,6 +49,23 @@ public class WindowsTest {
         }
         driver.findElement(By.linkText("baidu")).click();
         Thread.sleep(3000);
+    }
+
+    @Test
+    public void testCase() throws InterruptedException {
+        driver.get("file:///D:/selenium_html/index.html");
+        driver.findElement(By.linkText("Open new window")).click();
+        Thread.sleep(2000);
+        //得到当前窗口的set集合
+        Set<String> winHandles = driver.getWindowHandles();
+        //将set集合存入list对象
+        List<String> list = new ArrayList<String>(winHandles);
+        //切换到弹出的新窗口
+        driver.switchTo().window(list.get(1));
+        driver.findElement(By.linkText("baidu")).click();
+        //返回至原页面
+        driver.switchTo().window(list.get(0));
+        Thread.sleep(5000);
     }
 
     @AfterMethod
